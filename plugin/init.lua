a453dee825e12f7372b4f197ea1207103bd1eb0e
Aromatibus <aromablackcoffee@yahoo.co.jp>
2026-02-06 21:43:48 +0900

    a

--- plugin/init.lua
+++ plugin/init.lua
@@ -138,7 +138,7 @@ function M.setup(opts)
     " " ..
     "$cal_ic $year.$month.$day " ..
     "$clock_ic $time_24 " ..
-    "$loc_ic$location($country) " ..
+    "$loc_ic $location($country) " ..
     "$weather_ic $temp_ic($temp) " ..
     "$batt_ic$batt_num" ..
     " "
