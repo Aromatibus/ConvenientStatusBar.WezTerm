2ca220c4ebb06eb2f03bfba5ff18e54da44eb5a7
Aromatibus <aromablackcoffee@yahoo.co.jp>
2026-02-07 08:24:58 +0900

    a

--- plugin/init.lua
+++ plugin/init.lua
@@ -202,7 +202,7 @@ function M.setup(opts)
   local def_fmt =
     " $Cal_ic $Year.$Month.$Day($Week) $Clock_ic $Time24 " ..
     "$Loc_ic $City($Code) $Weather_ic $Temp_ic($Temp) " ..
-    "$Net_ic net_speed($Net_avg) $Batt_ic$Batt_num "
+    "$Net_ic $Net_speed($Net_avg) $Batt_ic$Batt_num "
 
   local cfg = {
     api_key      = opts.api_key,                  -- OpenWeatherMap APIキー
