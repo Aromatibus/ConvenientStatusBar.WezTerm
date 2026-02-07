861e8978f08d20dc32a9f7eff022ce1e5fcaa3f1
Aromatibus <aromablackcoffee@yahoo.co.jp>
2026-02-07 23:21:32 +0900

    wezterm.lua

--- doc/wezterm.lua
+++ doc/wezterm.lua
@@ -202,55 +202,55 @@ local ConvenientStatusBar = wezterm.plugin.require("https://github.com/aromatibu
 
 -- セットアップの実行
 ConvenientStatusBar.setup({
-    --weather_api_key         = "",
-    --weather_api_key         = "あなたのAPIキー",
-    weather_api_key         = "88989d7e3460606958812933b3209599",
-    startup_delay           = 5,
-    weather_lang            = "en",
-    weather_country         = "",
-    weather_city            = "",
-    weather_units           = "metric",
-    weather_update_interval = 600,
-    weather_retry_interval  = 30,
-    net_update_interval     = 3,
-    net_avg_samples         = 20,
-    separator_left          = "",
-    separator_right         = "",
-    color_text              = "#FFFFFF",
-    color_foreground        = "#7AA2F7",
-    color_background        = "#1A1B26",
-    format                  = 
-        " $user_ic $user " ..
-        "$cal_ic $year.$month.$day($week) $clock_ic $time24 " ..
-        "$loc_ic $city($code) $weather_ic $temp " ..
-        "$cpu_ic $cpu $mem_used_ic $mem_used $mem_free_ic $mem_free " ..
-        "$net_ic $net_speed($net_avg) " ..
-        "$batt_ic$batt_num ",
-    --week_str = {"日","一","二","三","四","五","六"}, -- Chinese
-    --week_str = {"Dom","Lun","Mar","Mié","Jue","Vie","Sáb"}, -- Spanish
-    --week_str = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"}, -- English
-    --week_str = {"रवि","सोम","मंगल","बुध","गुरु","शुक्र","शनि"}, -- Hindi
-    --week_str = {"রবি","সোম","मंगल","বুধ","বৃহ","শুক্র","শনি"}, -- Bengali
-    --week_str = {"Dom","2ª","3ª","4ª","5ª","6ª","Sáb"}, -- Portuguese
-    --week_str = {"ح","اث","ث","أر","خ","ج","س"}, -- Arabic
-    --week_str = {"Вс","Пн","Вт","Ср","Чт","Пт","Сб"}, -- Russian
-    --week_str = {"日","月","火","水","木","金","土"}, -- Japanese
-    --week_str = {"ਐਤ","ਸੋਮ","ਮੰਗਲ","ਬੁੱਧ","ਵੀਰ","ਸ਼ੁੱਕਰ","ਸ਼ਨੀ"}, -- Punjabi
-    --week_str = {"रवि","सोम","मंग","बुध","गुरू","शुक्र","शनि"}, -- Marathi
-    --week_str = {"ఆది","సోమ","మంగళ","బుధ","గురు","శుక్ర","శని"}, -- Telugu
-    --week_str = {"Ngah","Sen","Sel","Reb","Kem","Jum","Set"}, -- Javanese
-    --week_str = {"CN","T2","T3","T4","T5","T6","T7"}, -- Vietnamese
-    --week_str = {"So","Mo","Di","Mi","Do","Fr","Sa"}, -- German
-    --week_str = {"일","월","화","수","목","금","토"}, -- Korean
-    --week_str = {"Dim","Lun","Mar","Mer","Jeu","Ven","Sam"}, -- French
-    --week_str = {"Dom","Lun","Mar","Mer","Gio","Ven","Sab"}, -- Italian
-    --week_str = {"อา.","จ.","อ.","พ.","พฤ.","ศ.","ส."}, -- Thai
-    --week_str = {"Jpili","Jtt","Jnn","Jta","Alham","Jma","Jmos"}, -- Swahili
-    --week_str = {"Min","Sen","Sel","Rab","Kam","Jum","Sab"}, -- Indonesian
-    --week_str = {"nd","pn","wt","śr","cz","pt","sb"}, -- Polish
-    --week_str = {"Нд","Пн","Вт","Ср","Чт","Пт","Сб"}, -- Ukrainian
-    --week_str = {"sön","mån","tis","ons","tors","fre","lör"}, -- Swedish
-    --week_str = {"su","ma","ti","ke","to","pe","la"}, -- Finnish
+  --weather_api_key         = "",
+  --weather_api_key         = "あなたのAPIキー",
+  weather_api_key         = "88989d7e3460606958812933b3209599",
+  startup_delay           = 5,
+  weather_lang            = "en",
+  weather_country         = "",
+  weather_city            = "",
+  weather_units           = "metric",
+  weather_update_interval = 600,
+  weather_retry_interval  = 30,
+  net_update_interval     = 3,
+  net_avg_samples         = 20,
+  separator_left          = "",
+  separator_right         = "",
+  color_text              = "#FFFFFF",
+  color_foreground        = "#7AA2F7",
+  color_background        = "#1A1B26",
+  format                  = 
+    " $user_ic $user " ..
+    "$cal_ic $year.$month.$day($week) $clock_ic $time24 " ..
+    "$loc_ic $city($code) $weather_ic $temp " ..
+    "$cpu_ic $cpu $mem_used_ic $mem_used $mem_free_ic $mem_free " ..
+    "$net_ic $net_speed($net_avg) " ..
+    "$batt_ic$batt_num ",
+  --week_str = {"日","一","二","三","四","五","六"}, -- Chinese
+  --week_str = {"Dom","Lun","Mar","Mié","Jue","Vie","Sáb"}, -- Spanish
+  --week_str = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"}, -- English
+  --week_str = {"रवि","सोम","मंगल","बुध","गुरु","शुक्र","शनि"}, -- Hindi
+  --week_str = {"রবি","সোম","मंगल","বুধ","বৃহ","শুক্র","শনি"}, -- Bengali
+  --week_str = {"Dom","2ª","3ª","4ª","5ª","6ª","Sáb"}, -- Portuguese
+  --week_str = {"ح","اث","ث","أر","خ","ج","س"}, -- Arabic
+  --week_str = {"Вс","Пн","Вт","Ср","Чт","Пт","Сб"}, -- Russian
+  --week_str = {"日","月","火","水","木","金","土"}, -- Japanese
+  --week_str = {"ਐਤ","ਸੋਮ","ਮੰਗਲ","ਬੁੱਧ","ਵੀਰ","ਸ਼ੁੱਕਰ","ਸ਼ਨੀ"}, -- Punjabi
+  --week_str = {"रवि","सोम","मंग","बुध","गुरू","शुक्र","शनि"}, -- Marathi
+  --week_str = {"ఆది","సోమ","మంగళ","బుధ","గురు","శుక్ర","శని"}, -- Telugu
+  --week_str = {"Ngah","Sen","Sel","Reb","Kem","Jum","Set"}, -- Javanese
+  --week_str = {"CN","T2","T3","T4","T5","T6","T7"}, -- Vietnamese
+  --week_str = {"So","Mo","Di","Mi","Do","Fr","Sa"}, -- German
+  --week_str = {"일","월","화","수","목","금","토"}, -- Korean
+  --week_str = {"Dim","Lun","Mar","Mer","Jeu","Ven","Sam"}, -- French
+  --week_str = {"Dom","Lun","Mar","Mer","Gio","Ven","Sab"}, -- Italian
+  --week_str = {"อา.","จ.","อ.","พ.","พฤ.","ศ.","ส."}, -- Thai
+  --week_str = {"Jpili","Jtt","Jnn","Jta","Alham","Jma","Jmos"}, -- Swahili
+  --week_str = {"Min","Sen","Sel","Rab","Kam","Jum","Sab"}, -- Indonesian
+  --week_str = {"nd","pn","wt","śr","cz","pt","sb"}, -- Polish
+  --week_str = {"Нд","Пн","Вт","Ср","Чт","Пт","Сб"}, -- Ukrainian
+  --week_str = {"sön","mån","tis","ons","tors","fre","lör"}, -- Swedish
+  --week_str = {"su","ma","ti","ke","to","pe","la"}, -- Finnish
 })
 
 return config
